31ac8c5ee52acb1f00e0e3d5934e08737b94b611
Owner <Owner@DESKTOP-2TPH2RF>
2023-07-12 19:26:28 +0530

    Add project files.

---
+++ Deligate_Demo/Calculation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deligate_Demo
+{ 
+
+    public delegate int   MyDeligate(int x,int y);
+    public  class Calculation
+    {
+        public int Addition(int a,int b)
+        {
+            return a + b;
+        }
+    }
+    public delegate void Delegate1(int id, string name, double sallary);
+
+    public class Test
+    {
+        public void AcceptDetails(int id,string name, double sallary)
+        {
+            Console.WriteLine($"{id} {name} {sallary}");    
+        }
+    }
+}   
+
---
+++ Deligate_Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deligate_Demo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation c = new Calculation();
+            MyDeligate myDeligate = new MyDeligate(c.Addition);
+            Console.WriteLine(myDeligate.Invoke(20, 30));
+
+            Test t1 = new Test();
+            Delegate1 d1 = new Delegate1(t1.AcceptDetails);
+            d1.Invoke(1, "rahul", 25000);
+        }
+    }
+}
